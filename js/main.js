66ddba0da45e6fa808ddeca42aa6b5674b0785a7
Kia Lam <kia_lam@gspsf.com>
2014-03-20 17:23:11 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+url = 'http://www.json-generator.com/j/cqWlwQKSbm?indent=4'; //expires in 30 days as of 2/12/14
+var box;
+var artistArray;
+var x;
+var y;
+
+(function($) {
+	$(window).load(function() {
+		// get json
+		$.getJSON('json/artists.json', function (data) {
+				console.log('success getting JSON');
+				artistArray = data;
+				
+			}).fail( function (d, textStatus, error) {
+				console.error("getJSON failed, status: " + textStatus + ", error: "+ error);
+			});
+		// get artist info on click
+		$('body').on('cubeclick', function (evt) {
+			// var box = evt.box;
+			var data = artistArray;
+			var i = evt.artistId;
+			// console.log('position x: ');
+			// console.log(x);
+				//for (var i = 0; i < data.length; i++) {
+					// console.log(data[i].id);
+					//if (data[i].id == artistInfo) {
+						// console.log(data[i].name);
+						// console.log(box.y);
+						artistName = data[i].name;
+						artistCompany = data[i].company;
+						artistEmail = data[i].email;
+						// artistAbout = data[i].about;
+						// artistPic = data[i].picture;
+						openPopup();
+					//}
+				//}
+			// console.log('artistId:' + evt.artistId);
+        });
+		$('body').on('cubemove', function (evt) {
+			box = evt.box;
+
+			moveArtistInfo();
+        });
+	});
+})(jQuery);
+
+showAristInfo = function () {
+	$('#name').html(artistName);
+    $('#company').html(artistCompany);
+    $('#email').html(artistEmail);
+    // $('#about').html(artistAbout);
+    // $('#pic').attr("src",artistPic);
+};
+
+moveArtistInfo = function () {
+	var width = 900, height = 500;
+	var widthHalf = width / 2, heightHalf = height / 2;
+	var boxSize = {x:220,y:97};
+
+	posX = (box.x)-105; // added offset
+	posY = (-height+box.y-boxSize.y)-20; //added offset
+
+	$('.popup').css("left", posX);
+	$('.popup').css("top", posY);
+	
+};
+
+function openPopup() {
+    $('.popup').css('opacity', '0');
+    $('.popup').css('margin-top', '0');
+    $('.popup').show().animate({
+		opacity: 1,
+		marginTop: 5
+	});
+    showAristInfo();
+    moveArtistInfo();
+}
+
+function closePopup() {
+	$(".popup").animate({
+			opacity: 0,
+			marginTop: 0
+		}, function() {
+			$(this).hide();
+		});
+}
---
+++ webgl/js/main.js
@@ -0,0 +1,51 @@
+/**
+ * Returns a random number between min and max
+ */
+function getRandomArbitary (min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min and max
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var client, count = 0, humans = new Array();
+var player, done, ready = false;
+var canvas, context, image_from_canvas;
+
+$(document).ready(function() {
+	canvas = document.getElementById("canvasOutput");
+	canvas.width = 10;
+	canvas.height = 10;
+	context = canvas.getContext('2d');
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,10,10);
+
+	client = new Client( window, {
+		username: "website",
+		roomname: "adobelights",
+		host: "54.214.246.70",
+		//host: "localhost",
+		port: "8882",
+		secure: false,
+		debug: true
+	});
+
+	client.addEvent("move","any",function(msg, bid) {
+		var x = Math.floor(bid / 10);
+		var y = bid % 10;
+
+		console.log(msg, bid, x, y);
+		
+		var color = "rgba(" + msg + ")";
+		context.fillStyle = color;
+		context.fillRect(x,y,1,1);
+	});
+
+	client.connect();
+});
