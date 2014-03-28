56f4ccdc9151a8fae2b1fa7cef83c60abe279428
Mike Newell <mike_newell@gspsf.com>
2014-03-27 23:26:12 -0700

    added new iframe directory for the iframe on the behance page, going to make it the full version of the site without the video

---
+++ iframe/js/main.js
@@ -0,0 +1,78 @@
+// url = 'http://www.json-generator.com/j/cqWlwQKSbm?indent=4'; //expires in 30 days as of 2/12/14
+var box;
+var artistArray, artistName, artistWork, artistLoc;
+var x;
+var y;
+
+(function($) {
+	$(window).load(function() {
+		// get json
+		$.getJSON('http://54.203.249.154:5050/get/all', function (data) {
+			console.log('success getting JSON');
+			artistArray = data;
+			
+		}).fail( function (d, textStatus, error) {
+			console.error("getJSON failed, status: " + textStatus + ", error: "+ error);
+		});
+		// get artist info on click
+		$('body').on('cubeclick', function (evt) {
+			var data = artistArray;
+			var i = evt.artistId;
+			if(!data[i])return;
+			artistName = data[i].first_name+" "+data[i].last_name;
+			artistWork = data[i].behance;
+			artistLoc = data[i].physical_location;
+						
+			openPopup();
+        });
+		$('body').on('cubemove', function (evt) {
+			box = evt.box;
+			moveArtistInfo();
+        });
+	});
+})(jQuery);
+
+showAristInfo = function () {
+	$('#name').html(artistName);
+	if(artistWork.length > 6){
+		$('#work').attr('href', artistWork);
+		$('.link').html('View Work');
+	} else {
+		$('#work').attr('href', '');
+		$('.link').html('');
+	}
+    $('#location').html(artistLoc);
+};
+
+moveArtistInfo = function () {
+	var width = window.innerWidth, height = window.innerHeight;
+	var widthHalf = width / 2, heightHalf = height / 2;
+	var boxSize = {x:250,y:145};
+	if(!box)return;
+	posX = (box.x)-boxSize.x/2-10; // added offset
+	posY = (-height/2+box.y-boxSize.y)+100; //added offset
+
+	$('.popup').css("left", posX);
+	$('.popup').css("top", posY);
+	
+};
+
+function openPopup() {
+    $('.popup').css('opacity', '0');
+    $('.popup').css('margin-top', '0');
+    $('.popup').show().animate({
+		opacity: 1,
+		marginTop: 5
+	});
+    showAristInfo();
+    moveArtistInfo();
+}
+
+function closePopup() {
+	$(".popup").animate({
+			opacity: 0,
+			marginTop: 0
+		}, function() {
+			$(this).hide();
+		});
+}
